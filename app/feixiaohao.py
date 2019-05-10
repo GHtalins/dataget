9a5ae8c7e2efb684cab0afc493d6fde1a81723c7
GHtalins <920762125@qq.com>
2019-05-10 08:45:09 +0300

    test

--- app/feixiaohao.py
+++ app/feixiaohao.py
@@ -7,19 +7,21 @@ try:
     from app.package.network.dbopera import MongoDBOpera
     from app.package.network.webopera import WebOpera
     from app.package.format.webformat import WebFormat
+    from app.package.data.logger import Logger
     from concurrent.futures import ThreadPoolExecutor, as_completed
 except Exception as e:
     from .package.network import MongoDBOpera
     from .package.network import WebOpera
     from .package.format import WebFormat
-
+    from .package.data.logger import Logger
 
 
 code_collect,desc_collect,holds_collect= None,None,None
 holds_list,desc_list,holds_fail=[],[],[]
 FAIL_FLAG=False
-logging.basicConfig(format='%(asctime)s - %(pathname)s[line:%(lineno)d] - %(levelname)s: %(message)s',
-                    level=logging.DEBUG)
+log = Logger('dataget.log',level='debug')
+#logging.basicConfig(filename='dataget.log',filemode='a',format='%(asctime)s - %(pathname)s[line:%(lineno)d] - %(levelname)s: %(message)s',
+                  #  level=logging.DEBUG)
 
 def initDB():
     mo = MongoDBOpera()
@@ -64,7 +66,8 @@ def run_desc(code):
         MongoDBOpera().insert_to_collection(desc_collect,desc_item)
 
     except Exception as e:
-        logging.error(f"get_desc_info获取货币{code}描述数据失败", e)
+        log.logger.error(f"get_desc_info:get coin {code}description failed", e)
+        #log.logger.error(u"get_desc_info获取货币"+{code}+"描述数据失败", e)
     return code
 
 
@@ -74,17 +77,18 @@ def run_page(page):
         page_url = "https://dncapi.bqiapp.com/api/coin/web-coinrank?page=" + str(page) + "&type=0&pagesize=100&webp=1"
         wo = WebOpera(page_url)
         coin_list = wo.get_web_response()
-        logging.info(f"开始获取第{page}页货币列表")
+        log.logger.info(f"begin get page:{page} coin list")
+        #log.logger.info(u"开始获取第{"+page+"}页货币列表")
         if coin_list != '':
             for strs in coin_list["data"]:
                 obj=MongoDBOpera().collection_find_one(code_collect,{'CODE': strs["code"]})
                 if obj is None:  # 如果数据库中没有，则插入
                     MongoDBOpera().insert_to_collection(code_collect,{'CODE': strs["code"], 'name': strs["name"], 'page': page})
-                    logging.info(f"发现新币种:{strs['code']},插入CODE集合")
+                    log.logger.info(f"find new coin:{strs['code']},insert CODE collection")
         else:
-            logging.warning(f"第{page}页货币列表获取失败")
+            log.logger.warning(f"page:{page} coin list get failed")
     except Exception as e:
-        logging.error(f'run_page获取货币列表页{page}数据失败', e)
+        log.logger.error(f'run_page:get coin list page:{page} failed', e)
     return page
 
 #获取持币信息
@@ -104,9 +108,9 @@ def run_holds(code):
             holds_store_info["get_time"] = time.strftime("%Y-%m-%d", time.localtime())
             MongoDBOpera().insert_to_collection(holds_collect, holds_store_info)
         else:
-            logging.warning(f"币种{code}持币信息返回为空，获取失败")
+            log.logger.warning(f"coin:{code} holds info response empty")
     except Exception as e:
-        logging.error(f"run_holds获取货币{code}持币数据失败", e)
+        log.logger.error(f"run_holds:get coin:{code} holds info failed", e)
     return code
 
 
@@ -143,7 +147,7 @@ def run_wait(timesec):
         hour=deadline//3600
         minus=(deadline%3600)//60
         sec=deadline%60
-        logging.info(f"下次获取货币信息倒计时:{hour}时:{minus}分:{sec}秒")
+        log.logger.info(f"next run time count down:{hour}时:{minus}分:{sec}秒")
 
 
 
@@ -152,16 +156,15 @@ def main():
         try:
             localtime = time.strftime("%Y-%m-%d", time.localtime())
 
-            logging.info("开始获取币种基础信息")
+            log.logger.info(u'begin get coin basic info')
+
             executor = ThreadPoolExecutor(max_workers=24)
             all_task = [executor.submit(run_page, (page)) for page in range(1,25)]
             for future in as_completed(all_task,300):
                 data = future.result()
-            logging.info("币种基础信息获取完成")
+            log.logger.info("coin basic info get finished")
 
-
-
-            logging.info("开始获取币种描述信息")
+            log.logger.info("begin get coin description")
             desc_diff_list=get_list_diff(code_collect,desc_collect,"CODE",None,{"get_time": localtime})
             if len(desc_diff_list) != 0:
                 if len(desc_diff_list) > 50:
@@ -172,12 +175,11 @@ def main():
                 all_task_holds = [executor_holds.submit(run_desc, item) for item in desc_diff_list]
                 for future in as_completed(all_task_holds,1200):
                     code = future.result()
-                    logging.info(f"币种:{format(code)} 对应描述信息获取完成.")
+                    log.logger.info(f"coin:{format(code)} description get finished.")
                 #MongoDBOpera().insert_list_to_collection(desc_collect, desc_list)
-            logging.info("币种描述信息获取完成")
+            log.logger.info("all coins description finished")
 
-
-            logging.info("开始获取币种持币信息")
+            log.logger.info("begin get coin holds info")
 
             hold_diff_list = get_list_diff(code_collect, holds_collect, "CODE", None, {"get_time": localtime})
             if len(hold_diff_list) != 0:
@@ -188,13 +190,13 @@ def main():
                 all_task_holds = [executor_holds.submit(run_holds, item) for item in hold_diff_list]
                 for future in as_completed(all_task_holds, 1200):
                     code = future.result()
-                    logging.info(f"币种:{format(code)} 对应持币信息获取完成.")
+                    log.logger.info(f"coin:{format(code)} holds info get finished.")
                 #MongoDBOpera().insert_list_to_collection(holds_collect, desc_list)
-            logging.info("币种持币信息插入集合完成")
-            run_wait(7200)
+            log.logger.info("all coins holds info insert finished")
+            run_wait(3600)
         except Exception as e:
-            logging.error(u'main存在线程执行失败,本次不插入新补充的币种描述和持币信息', e)
-            run_wait(7200)
+            log.logger.error(u'main has some thread failed', e)
+            run_wait(3600)
 
 
 
